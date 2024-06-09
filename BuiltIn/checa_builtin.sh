e85bbdc696908f79a363f1a1d35fc41bf05da97f
andreza-vilar <andreza.farias@ccc.ufcg.edu.br>
2024-06-09 18:42:45 -0300

    refactor: mensagem de erro

--- BuiltIn/checa_builtin.sh
+++ BuiltIn/checa_builtin.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# Mensagem de erro...
+msg="É preciso informar um comando válido!"
+
+# Teste de quantidade de argumentos...
+[[ $# -ne 1 ]] && echo $msg && exit 1
+
+# Aqui nós executamos o comando "help"...
 help $1 &> /dev/null
 
+# E aqui nós testamos a saída com o comando "test"...
 [[ $? -eq 0 ]] && echo "$1 é interno!" || echo "$1 não é interno!"
 
 exit 0
