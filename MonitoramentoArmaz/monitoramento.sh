69b7f1ed388d024fd1e2a85a996f3572e2bb00ea
andreza-vilar <andreza.farias@ccc.ufcg.edu.br>
2024-06-26 01:06:00 -0300

    refactor: variaveis, senha, msg de alerta

--- MonitoramentoArmaz/monitoramento.sh
+++ MonitoramentoArmaz/monitoramento.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
-ESPACO=`df -h | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1`
-ESPACO_INODE=`df -hi | awk '{print $5}'  | grep -v IUse | sort -nr | awk -F % '{print $1}' | head -n1`
-ALERTA_RISCO="O disco esta prestes ficar a sem espaco por favor verificar \n\n `df -h ` \n\n `uname -a`"
-ALERTA_CHEIO="O disco encheu verificar com urgencia \n\n `df -h ` \n\n  `uname -a`"
-ALERTA_RISCO_INODE="O disco esta prestes a ficar sem espaco por favor verificar \n\n `df -hi ` \n\n `uname -a`"
-ALERTA_CHEIO_INODE="O disco encheu verificar com urgencia \n\n `df -hi ` \n\n  `uname -a`"
-SUBJECT_1="AVISO DE DISCO" #TITULO DO EMAIL
-SUBJECT_2="DISCO LOTADO" #TITULO DO EMAIL
-SUBJECT_1_I="AVISO DE DISCO" #TITULO DO EMAIL
-SUBJECT_2_I="DISCO LOTADO" # TITULO DO EMAIL
-EMAIL_DST="emaildestino@seuemail.com.br" #ALTERAR PARA SEU EMAIL
-EMAIL_ORI="emailorigem@seumeial.com.br" #ALTERAR PARA O EMAIL QUE ENVIARA
-SMTP="smtp.seudominio.com.br" #ALTERAR PARA O SMTP DO SEU SERVER
-USR="smtp@seudominio.com.br"  #ALTERAR PARA O USUARIO DO EMAIL QUE ENVIARA
-SENHA="suasenha" #ALTERAR PARA A SENHA DO USUARIO QUE ENVIARA
-TLS="tls=yes" #CASO SEU EMAIL NAO TENHA TLS ALTERE PARA no 
+# Configurações de alerta e email
+EMAIL_DST="emaildestino@seuemail.com.br"
+EMAIL_ORI="emailorigem@seumeial.com.br"
+SMTP="smtp.seudominio.com.br"
+USR="smtp@seudominio.com.br"
+SENHA="suasenha"
+TLS="tls=yes"
 
+# Função para enviar email
+enviar_email() {
+    local subject="$1"
+    local message="$2"
+    sendEmail -f "$EMAIL_ORI" -t "$EMAIL_DST" -u "$subject" -m "$message" -s "$SMTP:587" -xu "$USR" -xp "$SENHA" -o "$TLS"
+}
 
-# Verifica espaco em disco
+# Verifica espaço em disco
+ESPACO=$(df -h | awk '{print $5}' | grep -v Use | sort -nr | awk -F % '{print $1}' | head -n1)
+ESPACO_INODE=$(df -hi | awk '{print $5}' | grep -v IUse | sort -nr | awk -F % '{print $1}' | head -n1)
+
+# Mensagens de alerta
+ALERTA_RISCO="O disco está prestes a ficar sem espaço. Por favor, verificar.\n\n$(df -h)\n\n$(uname -a)"
+ALERTA_CHEIO="O disco encheu. Verificar com urgência.\n\n$(df -h)\n\n$(uname -a)"
+ALERTA_RISCO_INODE="O disco está prestes a ficar sem espaço (INODE). Por favor, verificar.\n\n$(df -hi)\n\n$(uname -a)"
+ALERTA_CHEIO_INODE="O disco encheu (INODE). Verificar com urgência.\n\n$(df -hi)\n\n$(uname -a)"
+
+# Envio de emails baseado no espaço em disco
 case $ESPACO in
-        9[1-9]) #Caso o disco esteja entre 91 a 99 ele manda um email avisando
-        sendEmail -f $EMAIL_ORI -t $EMAIL_DST -u $SUBJECT_1 -m "$ALERTA_RISCO" -s $SMTP:587 -xu $USR -xp $SENHA -o $TLS;;
-        100)# Caso encha tb
-        sendEmail -f $EMAIL_ORI -t $EMAIL_DST -u $SUBJECT_2 -m "$ALERTA_CHEIO" -s $SMTP:587 -xu $USR -xp $SENHA -o $TLS;;
+    9[1-9]|100)
+        if [ "$ESPACO" -eq 100 ]; then
+            enviar_email "DISCO LOTADO" "$ALERTA_CHEIO"
+        else
+            enviar_email "AVISO DE DISCO" "$ALERTA_RISCO"
+        fi
+        ;;
 esac
 
+# Envio de emails baseado no espaço INODE
 case $ESPACO_INODE in
-        9[1-9]) #Caso o disco esteja entre 91 a 99 ele manda um email avisando
-        sendEmail -f $EMAIL_ORI -t $EMAIL_DST -u $SUBJECT_1_I -m "$ALERTA_RISCO_INODE" -s $SMTP:587 -xu $USR -xp $SENHA -o $TLS;;
-        100)# Caso encha tb
-        sendEmail -f $EMAIL_ORI -t $EMAIL_DST -u $SUBJECT_2_I  -m "$ALERTA_CHEIO_INODE"-s $SMTP:587 -xu $USR -xp $SENHA -o $TLS;;
+    9[1-9]|100)
+        if [ "$ESPACO_INODE" -eq 100 ]; then
+            enviar_email "DISCO LOTADO (INODE)" "$ALERTA_CHEIO_INODE"
+        else
+            enviar_email "AVISO DE DISCO (INODE)" "$ALERTA_RISCO_INODE"
+        fi
+        ;;
 esac
+
